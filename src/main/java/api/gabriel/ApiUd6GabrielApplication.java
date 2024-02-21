84f28ebfc1c0a05e9c79261addaba640d3db6b5d
gabriel <gabriel.gandara@iessanmamede.com>
2024-02-21 15:16:38 +0100

    Proyecto Empezado

---
+++ src/main/java/api/gabriel/ApiUd6GabrielApplication.java
@@ -0,0 +1,13 @@
+package api.gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6GabrielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUd6GabrielApplication.class, args);
+    }
+
+}
